db199f886a6f69f6c46010289bb9b6eeba3af835
Josephine <bluejello@gmail.com>
2016-07-21 15:57:30 -0700

    Attempt to implement stringifyJSON function
    I tried account for all types stringifyable and skip types that can't
    be stringified.  However, it seems that the code is still broken and
    is detecting an error with trying to convert undefined or null to object.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,51 @@
 
 // but you don't so you're going to write it from scratch:
 
+//obj = {"a": 1, "b": [1,2,3], "c": function(){}, "d": undefined};
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var type = typeof obj;
+  var string = "";
+
+  //if type is object
+  if (type === "object"){ 
+  	if (Array.isArray(obj)){ //if object is an array
+  		string += "[";
+  		for (var i=0; i<obj.length; i++){
+  			if (i === obj.length - 1){ //if last element, no comma
+  				string += stringifyJSON(obj[i]);
+  			} else { //otherwise, include comma
+  				string += stringifyJSON(obj[i]) + ",";
+  			}
+  		}
+  		return string += "]";
+  	} else { //if object is not an array
+  		var keys = Object.keys(obj); //keys is an array of keys in our object
+  		string += "{";
+  		for (var key in obj){
+  			if (typeof obj[key] !== "function" && typeof obj[key] !== "undefined"){
+  				//skip functions and undefined values
+  				if (key === keys[keys.length-1]){ //if last key, no comma
+	  				string += String(key) + ":" + stringifyJSON(obj[key]);
+	  			} else { //otherwise, include comma
+	  				string += String(key) + ":" + stringifyJSON(obj[key]) + ",";
+	  			}
+	  		}
+  		}
+  		return string += "}";
+  	}
+  }
+  /* HINT: JSON does not allow you to stringify functions or undefined values, so you should skip those key/value pairs. */
+
+  //if type is function or undefined
+  if (type === "function" || type === "undefined"){
+  	return "";
+  }
+  //if type is number or boolean
+  if (type === "number" || type === "boolean"){
+  	return String(obj);
+  }
+  //if type is string
+  if (type === "string"){
+  	return obj;
+  }
 };
